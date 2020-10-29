db725fcde8bba702d010c7b491d5d6b952ff01a4
Emene-Pesh <emeneche@gmail.com>
2020-10-29 17:40:58 +0100

    Implemented piece functions
    Implementation of PiecaAt, MovePiece and PlacePieceAt

--- exercises/chess/model/board/board.go
+++ exercises/chess/model/board/board.go
@@ -1,6 +1,7 @@
 package board
 
 import (
+	"fmt"
 	"github.com/Emene-Pesh/4eail40_2020/exercises/chess/model/coord"
 	"github.com/Emene-Pesh/4eail40_2020/exercises/chess/model/piece"
 )
@@ -15,18 +16,44 @@ func (c *Classic) String() string {
 // PieceAt retrievves piece at give coordinates.
 // Returns nil if no piece was found.
 func (c *Classic) PieceAt(at coord.ChessCoordinates) piece.Piece {
-	panic("not implemented") // TODO: Implement
+	x,_ := at.Coord(0)
+	y,_ := at.Coord(1)
+
+	if x>8 || y>8 {
+		return nil
+	}
+	if c[x][y] != nil {
+		return c[x][y]
+	}
+	return nil
 }
 
 // MovePiece moves a piece from given coordinates to
 // given coordinates.
 // Returns an error if destination was occupied.
 func (c *Classic) MovePiece(from coord.ChessCoordinates, to coord.ChessCoordinates) error {
-	panic("not implemented") // TODO: Implement
+	if c.PieceAt(from) == nil{
+		return fmt.Errorf("No pieces to move")
+	}
+	if c.PieceAt(to) != nil{
+		return fmt.Errorf("occupied")
+	}
+	x,_ := from.Coord(0)
+	y,_ := from.Coord(1)
+	piece := c[x][y]
+	c[x][y]=nil
+	return c.PlacePieceAt(piece,to)
+
 }
 
 // PlacePieceAt places a given piece at given location.
 // Returns an error if destination was occupied.
 func (c *Classic) PlacePieceAt(p piece.Piece, at coord.ChessCoordinates) error {
-	panic("not implemented") // TODO: Implement
+	if c.PieceAt(at) != nil{
+		return fmt.Errorf("occupied")
+	}
+	x, _ := at.Coord(0)
+	y, _ := at.Coord(1)
+	c[x][y] = p
+	return nil
 }
